c1d47b56e3c122b12b0651f7f7905ac57e2af660
Juan Castillo <ing.juanpcc@gmail.com>
2021-07-30 19:48:01 -0400

    tarea 2 tiempo corregido

--- reloj/views.py
+++ reloj/views.py
@@ -1,12 +1,15 @@
 
 # Create your views here.
 from django.shortcuts import render, HttpResponse, redirect
-from time import gmtime, strftime
+from time import gmtime, localtime, strftime
+from django.utils import timezone
+
     
 def index(request):
+    print(timezone.now())
     context = {
-        "date": strftime("%b %d, %Y", gmtime()),
-        "time": strftime("%I:%M %p %X", gmtime())
+        "date": strftime("%b %d, %Y", localtime()),
+        "time": strftime("%I:%M %p %X", localtime()),
     }
     return render(request,'index.html', context)
 
